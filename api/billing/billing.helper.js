ab60c7e9c2f032aa66d9a3560d6773ae9db60c1c
Muhammad Royyan <pandu.royyan@zetta-intern.space>
2024-08-30 15:51:41 +0700

    refactor calculate amount for each payer function to make precise

--- api/billing/billing.helper.js
+++ api/billing/billing.helper.js
@@ -36,35 +36,35 @@ const GenerateBillingBasedOnPayer = async (studentId, registrationProfileId, pay
   const isPayerOnlyStudent = payer.every((p) => p.type === 'Student') && payer.length === 1;
 
   // *************** distributed amount for each payer ***************
-  const amountForEachPayer = CalculateAmountForEachPayer(payer, totalAmount, termAmount);
+  const termAmountForEachPayer = CalculateTermAmountForEachPayer(payer, termAmount);
 
   const billingData = [];
 
   //*************** this block only executed when payer is mix or only financial support
   for (let i = 0; i < payer.length; i++) {
     if (payer[i].type === 'FinancialSupport') {
-      const termIds = await GenerateTerms(termPayments, amountForEachPayer.termAmount[i]);
+      const termIds = await GenerateTerms(termPayments, termAmountForEachPayer[i]);
       billingData.push({
         student_id: studentId,
         registration_profile_id: registrationProfileId,
         payer: payer[i].payer_id,
-        total_amount: amountForEachPayer.totalAmount[i],
+        total_amount: termAmountForEachPayer[i],
         paid_amount: 0,
-        remaining_due: 0,
+        remaining_due: termAmountForEachPayer[i],
         term_ids: termIds,
       });
     }
 
     if (payer[i].type === 'Student' && !isPayerOnlyStudent) {
       const depositId = await GenerateDeposit(termPayments, deposit);
-      const termIds = await GenerateTerms(termPayments, amountForEachPayer.termAmount[i]);
+      const termIds = await GenerateTerms(termPayments, termAmountForEachPayer[i]);
       billingData.push({
         student_id: studentId,
         registration_profile_id: registrationProfileId,
         payer: payer[i].payer_id,
-        total_amount: amountForEachPayer.totalAmount[i],
+        total_amount: termAmountForEachPayer[i] + deposit,
         paid_amount: 0,
-        remaining_due: amountForEachPayer.totalAmount[i],
+        remaining_due: termAmountForEachPayer[i] + deposit,
         deposit_id: depositId,
         term_ids: termIds,
       });
@@ -79,9 +79,9 @@ const GenerateBillingBasedOnPayer = async (studentId, registrationProfileId, pay
       student_id: studentId,
       registration_profile_id: registrationProfileId,
       payer: studentId,
-      total_amount: 0,
+      total_amount: deposit,
       paid_amount: 0,
-      remaining_due: 0,
+      remaining_due: deposit,
       deposit_id: depositId,
     });
   }
@@ -135,6 +135,12 @@ const GenerateTerms = async (termPayments, termAmount) => {
       remaining_amount: parseFloat((termAmount * percentage).toFixed(2)),
     });
   });
+
+  const totalDistributed = termData.reduce((acc, curr) => acc + curr.amount, 0);
+  const difference = parseFloat((termAmount - totalDistributed).toFixed(2));
+  if (difference !== 0) {
+    termData[termData.length - 1].amount += difference;
+  }
 
   const terms = await Term.insertMany(termData);
   return terms.map((term) => term._id);
@@ -205,23 +211,21 @@ const UpdateDepositBillingId = async (billings) => {
  * @param {Number} payers.cost_coverage - cost coverage for each payer
  * @param {number} totalAmount - The total amount to be distributed among payers.
  * @param {number} termAmount - The term amount to be distributed among payers.
- * @returns {{ totalAmount: number[], termAmount: number[] }} An object containing arrays for totalAmount and termAmount, each distributed precisely among payers.
+ * @returns {number[]} An arrays for totalAmount and termAmount, each distributed precisely among payers.
  */
-const CalculateAmountForEachPayer = (payers, totalAmount, termAmount) => {
-  const distributeAmount = (amount, payers) => {
-    return payers.map((payer) => {
-      const coveragePercentage = payer.cost_coverage / 100;
-      return parseFloat((amount * coveragePercentage).toFixed(2));
-    });
-  };
+const CalculateTermAmountForEachPayer = (payers, termAmount) => {
+  const amountForEachPayer = payers.map((payer) => {
+    const coveragePercentage = payer.cost_coverage / 100;
+    return parseFloat((termAmount * coveragePercentage).toFixed(2));
+  });
 
-  const distributedTotalAmount = distributeAmount(totalAmount, payers);
-  const distributedTermAmount = distributeAmount(termAmount, payers);
+  const totalDistributed = amountForEachPayer.reduce((acc, curr) => acc + curr, 0);
+  const difference = parseFloat((termAmount - totalDistributed).toFixed(2));
+  if (difference !== 0) {
+    amountForEachPayer[amountForEachPayer.length - 1] += difference;
+  }
 
-  return {
-    totalAmount: distributedTotalAmount,
-    termAmount: distributedTermAmount,
-  };
+  return amountForEachPayer;
 };
 
 /**
@@ -314,7 +318,6 @@ const PayDeposit = async (deposit, paidAmount) => {
  */
 const PayTerms = async (terms, paidAmount) => {
   let remainder = parseFloat(paidAmount).toFixed(2);
-  console.log('first remainder', remainder);
 
   for (let i = 0; i < terms.length; i++) {
     if (terms[i].remaining_amount > 0 && remainder > 0) {
